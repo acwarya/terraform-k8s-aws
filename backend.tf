3926421e13b6e835b1f4d2d98161cfb0f1010d35
Ashish Chettri <ashish.chettri@gmail.com>
2026-01-19 15:54:12 -0800

    Add S3 backend configuration with bucket terraform-state-k8s-ac-20260119155141

---
+++ backend.tf
@@ -0,0 +1,9 @@
+﻿terraform {
+  backend "s3" {
+    bucket         = "terraform-state-k8s-ac-20260119155141"
+    key            = "k8s-cluster/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "terraform-state-lock"
+  }
+}
